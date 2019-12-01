16c01f4d4290094580494f6e136129a5ad436cdf
Miguel Machado <miiguellmp@gmail.com>
2019-12-01 12:38:57 -0300

    finishing Day one

--- Day 1 The Tyranny of the Rocket Equation/index.js
+++ Day 1 The Tyranny of the Rocket Equation/index.js
@@ -1,13 +1,40 @@
-const fs = require("fs");
-const readline = require("readline");
+var fs = require("fs");
+const inputFile = "input.txt";
 
-const readInterface = readline.createInterface({
-  input: fs.createReadStream("./input.txt"),
-  output: process.stdout,
-  console: false
-});
+// Part One
+function partOne() {
+  let total_fuel = 0;
+  // Use trim to avoid any extra newlines being parsed as NaN by parseInt
+  // Create an array of the masses from the input file
+  const masses = fs
+    .readFileSync(inputFile, { encoding: "utf-8" })
+    .trim()
+    .split("\n");
+  for (let i = 0; i < masses.length; i++) {
+    total_fuel += Math.floor(parseInt(masses[i]) / 3) - 2;
+  }
+  return total_fuel;
+}
 
-readInterface.on("line", function(line) {
-  const fuel = Math.floor(line / 3) - 2;
-  console.log(`${fuel}`);
-});
+// Part Two
+function partTwo() {
+  let total_fuel = 0;
+  // Use trim to avoid any extra newlines being parsed as NaN by parseInt
+  // Create an array of the masses from the input file
+  const masses = fs
+    .readFileSync(inputFile, { encoding: "utf-8" })
+    .trim()
+    .split("\n");
+  for (let i = 0; i < masses.length; i++) {
+    let fuel = Math.floor(parseInt(masses[i]) / 3) - 2;
+    // Keep adding fuel until the fuel calculation is negative
+    while (fuel > 0) {
+      total_fuel += fuel;
+      fuel = Math.floor(fuel / 3) - 2;
+    }
+  }
+  return total_fuel;
+}
+
+console.log(partOne());
+console.log(partTwo());
