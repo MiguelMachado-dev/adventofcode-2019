29c766185c23897332dda1da9a4e9bf165b39ca4
Miguel Machado <miiguellmp@gmail.com>
2019-12-01 14:35:46 -0300

    spliting solutions into 2 files

---
+++ day-1-tyranny-of-rocket-equation/partOne.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+const inputFile = "input.txt";
+
+// Part One
+function partOne() {
+  let total_fuel = 0;
+  // Use trim to avoid any extra newlines being parsed as NaN by parseInt
+  // Create an array of the masses from the input file
+  let masses = fs
+    .readFileSync(inputFile, { encoding: "utf-8" })
+    .trim()
+    .split("\n");
+  for (let i = 0; i < masses.length; i++) {
+    total_fuel += Math.floor(parseInt(masses[i]) / 3) - 2;
+  }
+  return total_fuel;
+}
+
+module.exports = partOne;
+
+console.log(partOne());
